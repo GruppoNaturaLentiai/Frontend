6682f1c1351b453bcdaa5f273384d703fed9ef0f
Andrea Nicolai <andrea.nicolai@piratech.io>
2025-01-13 17:49:54 +0100

    more

---
+++ src/components/portable-text-components/styled.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import * as T from "../typography"
+
+export const ImageWrapper = styled.div<{ position?: "left" | "right" | "center" }>`
+  display: flex;
+  flex-direction: column;
+  align-items: ${({ position }) => {
+    switch (position) {
+      case 'left':
+        return 'flex-start';
+      case 'right':
+        return 'flex-end';
+      default:
+        return 'center';
+    }
+  }};
+  text-align: ${({ position }) => (position === 'center' ? 'center' : 'inherit')};
+`;
+
+export const ImgDescription = styled(T.P4)`
+  font-style: italic;
+  text-align: center;
+  margin-block-start: 0.5em;
+`
