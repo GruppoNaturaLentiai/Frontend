59ebdee2fc993dcf9826c9c5810c8d5b15a4e3f5
Andrea Nicolai <andrea.nicolai@piratech.io>
2025-05-08 11:13:16 +0200

    after buying the domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const remoteGraphqlURL =
   "https://ze02ljgl.api.sanity.io/v2023-08-01/graphql/post-blog/default" as const
 
-export const pathPrefix = "/Frontend" as const
+// export const pathPrefix = "/Frontend" as const // old when hosted in https://grupponaturalentiai.github.io/Frontend
+export const pathPrefix = "/" // actual having bought the domain
